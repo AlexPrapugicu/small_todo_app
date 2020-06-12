7934965e80e38baf19f163c95a1d77f56dd81ea2
Prapugicu Alex <alexprapugicu@gmail.com>
2020-06-12 17:32:47 +0300

    added Header component
    Used material UI and Moment libraries to make a nice realtime clock and a title for the app

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Moment from "react-moment";
 
 function Header() {
 
-    return(<div>
-        Header Component
-    </div>);
+    return (
+        <div>
+            <Typography
+                variant="h5"
+                style={{padding: 20}}>
+                To Do's
+            </Typography>
+            <div style={{display:'inline-flex'}}>
+                <p>Current Time: </p>
+                <p>
+                    <Moment
+                        format={"DD-MMM-YYY HH:mm:ss"}
+                        interval={1000}
+                        style={{justifyContent:"center"}}>
+                    </Moment>
+                </p>
+            </div>
+        </div>);
 }
 
 export default Header;
